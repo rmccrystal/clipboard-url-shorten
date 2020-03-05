5b98f9f4ca3814ac7d281758e80959a71fefde02
Ryan McCrystal <rrmccrystal@gmail.com>
2020-03-05 12:23:16 -1000

    Started project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use urlshortener::{client::UrlShortener, providers::Provider};
+use clipboard::{ClipboardContext, ClipboardProvider};
+use std::fmt::Error;
+
+// The service which will shorten our URLs
+static PROVIDER: Provider = Provider::VGd;
+
 fn main() {
-    println!("Hello, world!");
+	let us = UrlShortener::new().unwrap();
+	clipboard_loop(us).unwrap();
+}
+
+// The loop which will modify our clipboard
+fn clipboard_loop(us: UrlShortener) -> Result<(), Error> {
+	// The last thing the clipboard was set to
+	// If this variable is different, the clipboard data will be handled
+	let mut last_clipboard = String::new();
+	let mut provider = ClipboardProvider::new()?;
+	loop {
+		let clipboard_text = provider.get_contents()?;
+		if clipboard_text != last_clipboard {   // If we have new content in the clipboard
+			last_clipboard = clipboard_text;    // Update the last clipboard to the current clipboard
+			let new_clipboard = handle_clipboard_change(clipboard, &us)?;
+			provider.set_contents(new_clipboard)?
+		}
+	}
+}
+
+// This function is ran whenever the clipboard is changed
+// Takes the clipboard text and the url shortener object
+// and returns the new clipboard data
+fn handle_clipboard_change(clipboard_text: String, us: &UrlShortener) -> Result<String, Error> {
+	return Ok(Strings::new("test"))
 }
